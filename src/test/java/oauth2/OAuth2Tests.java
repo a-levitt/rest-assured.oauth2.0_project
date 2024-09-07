7b51ee1bb4e5368f83b5aa28b06d21522930b044
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-07 16:37:00 +0200

    Create template for access code request

--- src/test/java/oauth2/OAuth2Tests.java
+++ src/test/java/oauth2/OAuth2Tests.java
@@ -9,16 +9,32 @@ import static io.restassured.RestAssured.*;
 public class OAuth2Tests {
     public static void main(String[] args) {
 
+        String clientId = "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com";
 
-        //2. Get access token
+        // 3. Get access code
+        String accessCodeResponse =
+        given()
+                .queryParam("scope", "https://www.googleapis.com/auth/userinfo.email")
+                .queryParam("auth_url", "https://accounts.google.com/o/oauth2/v2/auth")
+                .queryParam("client_id", clientId)
+                .queryParam("response_type", "code")
+                .queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+        .when()
+                .get("https://accounts.google.com/o/oauth2/v2/auth")
+        .asString()
+        ;
+
+
+        // 2. Get access token
         String accessTokenResponse =
         given()
                 .queryParam("code", "")
-                .queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .queryParam("client_id", clientId)
                 .queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
                 .queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
                 .queryParam("grant_type", "authorization_code")
         .when()
+                .log().all()
                 .post("https://www.googleapis.com/oauth2/v4/token")
         .asString()
         ;
@@ -30,6 +46,7 @@ public class OAuth2Tests {
         given()
                 .queryParam("access_token", accessToken)
         .when()
+                .log().all()
                 .get("https://rahulshettyacademy.com/getCourse.php")
         .asString()
         ;
